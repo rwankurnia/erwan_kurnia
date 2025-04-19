8df280b1f984583fc0dc2bdf44655a473384b5c8
rwankurnia <rwankurnia@gmail.com>
2025-04-19 16:54:06 +0700

    tugas-01

--- lib/tugas-week-01/tugas-01.dart
+++ lib/tugas-week-01/tugas-01.dart
@@ -1,91 +1,55 @@
 import 'package:flutter/material.dart';
 
-class LoginApp extends StatefulWidget {
-  const LoginApp({super.key});
-
-  @override
-  State<LoginApp> createState() => _LoginAppState();
+class LoginPage extends StatelessWidget {
+  
+    @override
+    Widget build(BuildContext context) {
+        return Scaffold(
+            appBar: AppBar(
+              centerTitle: true,
+                backgroundColor: const Color.fromARGB(255, 83, 127, 184),
+                title: Text('Login',
+                  style: TextStyle(
+                    color: const Color.fromARGB(233, 167, 216, 19),
+                    fontSize: 36,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+            ),
+            body: Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                        TextField(
+                            decoration: InputDecoration(
+                                labelText: 'Email',
+                                border: OutlineInputBorder(),
+                            ),
+                        ),
+                        SizedBox(height: 16),
+                        TextField(
+                            obscureText: true,
+                            decoration: InputDecoration(
+                                labelText: 'Password',
+                                border: OutlineInputBorder(),
+                            ),
+                        ),
+                        SizedBox(height: 24),
+                        ElevatedButton(
+                            onPressed: () {
+                            },
+                            child: Text('Login'),
+                        ),
+                    ],
+                ),
+            ),
+        );
+    }
 }
 
-class _LoginAppState extends State<LoginApp> {
-
-  final _formKey = GlobalKey<FormState>();
-  TextEditingController usernameController = TextEditingController();
-  TextEditingController passwordController = TextEditingController();
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Login Page',
-            style: TextStyle(
-              color: const Color.fromARGB(233, 237, 239, 241),
-              fontSize: 36,
-              fontWeight: FontWeight.bold,
-            ),
-          ),
-          backgroundColor: const Color.fromARGB(255, 101, 181, 234),
-      ),
-      backgroundColor: const Color.fromARGB(255, 161, 197, 221),
-      body: Form(
-        key: _formKey,
-        child: Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 9, vertical: 27),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [ 
-              Padding(
-                padding:
-                  const EdgeInsets.symmetric(horizontal: 9, vertical: 27),
-                  child: TextFormField(
-                    controller: usernameController,
-                    decoration: const InputDecoration(
-                      fillColor: Color.fromARGB(255, 216, 235, 221),
-                      filled: true,
-                      border: OutlineInputBorder(), labelText: "Username"),
-                    validator: (value) {
-                      if (value == null || value.isEmpty) {
-                        return 'Masukkan username';
-                      }
-                      return null;
-                    },
-                  ),
-              ),
-              Padding(
-                padding:
-                  const EdgeInsets.symmetric(horizontal: 9, vertical: 27),
-                  child: TextFormField(
-                    controller: passwordController,
-                    obscureText: true,
-                    decoration: const InputDecoration(
-                      fillColor: Color.fromARGB(255, 216, 235, 221),
-                      filled: true,
-                      border: OutlineInputBorder(), labelText: "Password"),
-                    validator: (value) {
-                      if (value == null || value.isEmpty) {
-                        return 'Masukkan Password';
-                      }
-                      return null;
-                    },
-                  ),
-              ),
-              ElevatedButton(
-                onPressed: () {
-                  if (_formKey.currentState!.validate()) {
-                    ScaffoldMessenger.of(context).showSnackBar(
-                      const SnackBar(content: Text('OTW HALAMAN HOME PAGE')),
-                    );
-                  }
-                },
-                child: Image.asset(
-                      'assets/Login.png',
-                      width: 99,
-                      ),
-              ),
-            ]
-          ),
-        ),
-      ),
-    );
-  }
+void main() {
+    runApp(MaterialApp(
+        home: LoginPage(),
+    ));
 }
