2c052ea246546f7c803fd664b85d53a3fe89d112
rwankurnia <rwankurnia@gmail.com>
2025-04-21 20:45:21 +0700

    tugasweek1

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,13 @@
-import 'package:erwan_kurnia/tugas-week-01/tugas-01.dart';
+// import 'package:erwan_kurnia/tugas-week-01/tugas-01.dart';
 // import 'package:erwan_kurnia/day-4/layout_page.dart';
 // import 'package:erwan_kurnia/day-4/listview_page.dart';
 // import 'package:erwan_kurnia/day-4/listview_builder_page.dart';
 // import 'package:erwan_kurnia/day-4/listview_separated_page.dart';
+// import 'package:erwan_kurnia/day-4/gridview_page.dart';
+// import 'package:erwan_kurnia/day-4/gridview_builder_page.dart';
+// import 'package:erwan_kurnia/day-4/gridview_count_page.dart';
+// import 'package:erwan_kurnia/day-4/gridview_builder_page.dart';
+import 'package:erwan_kurnia/tugas-week-01/tugas-01.dart';
 import 'package:flutter/material.dart';
 void main() {
   runApp(const MyApp());
@@ -16,7 +21,19 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: '',
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: const Color.fromARGB(255, 255, 255, 255)),
+        appBarTheme: const AppBarTheme(
+          centerTitle: true,
+          backgroundColor: Colors.green,
+          surfaceTintColor: Colors.green,
+          titleTextStyle: TextStyle(
+            color: Colors.white,
+            fontSize: 36,
+            fontWeight: FontWeight.bold
+          ),
+          iconTheme: IconThemeData(
+            color: Colors.white,
+          ),
+        ),
       ),
       home: LoginPage()
     );
--- lib/tugas-week-01/tugas-01.dart
+++ lib/tugas-week-01/tugas-01.dart
@@ -2,49 +2,39 @@ import 'package:flutter/material.dart';
 
 class LoginPage extends StatelessWidget {
   
-    @override
-    Widget build(BuildContext context) {
-        return Scaffold(
-            appBar: AppBar(
-              backgroundColor: Colors.blue,
-              title: Center(child: 
-                Text('Login',
-                  style: TextStyle(
-                    color: Colors.green,
-                    fontSize: 36,
-                    fontWeight: FontWeight.bold,
-                  ),
-                ),
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Login'),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(54.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            TextField(
+              decoration: InputDecoration(
+                labelText: 'Email',
+                border: OutlineInputBorder(),
               ),
             ),
-            body: Padding(
-                padding: const EdgeInsets.all(54.0),
-                child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                        TextField(
-                            decoration: InputDecoration(
-                                labelText: 'Email',
-                                border: OutlineInputBorder(),
-                            ),
-                        ),
-                        SizedBox(height: 16),
-                        TextField(
-                            obscureText: true,
-                            decoration: InputDecoration(
-                                labelText: 'Password',
-                                border: OutlineInputBorder(),
-                            ),
-                        ),
-                        SizedBox(height: 24),
-                        ElevatedButton(
-                            onPressed: () {
-                            },
-                            child: Text('Login'),
-                        ),
-                    ],
-                ),
+            SizedBox(height: 16),
+            TextField(
+              obscureText: true,
+              decoration: InputDecoration(
+                labelText: 'Password',
+                border: OutlineInputBorder(),
+              ),
             ),
-        );
-    }
+            SizedBox(height: 24),
+            ElevatedButton(
+              onPressed: () {},
+              child: Text('Login'),
+            ),
+          ],
+       ),
+      ),
+    );
+  }
 }
